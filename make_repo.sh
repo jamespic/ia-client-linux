be781e9b28f5e31907f3a55371436983b683e092
James Pickering <james.pickering@airelogic.com>
2019-04-12 14:39:21 +0100

    Add uncompressed packages and sources

--- make_repo.sh
+++ make_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make_release() {
-  rm Release Release.gpg
+  rm Release Release.gpg InRelease
   apt-ftparchive release . > /tmp/Release
   cp /tmp/Release Release
   gpg --clearsign -o InRelease Release
@@ -10,13 +10,13 @@ make_release() {
 for packagedir in dists/bionic/unofficial/binary-*
 do
   pushd "$packagedir"
-  apt-ftparchive packages . | gzip > Packages.gz
+  apt-ftparchive packages . | tee Packages | gzip > Packages.gz
   make_release
   popd
 done
 
 pushd dists/bionic/unofficial/source
-apt-ftparchive sources . | gzip > Sources.gz
+apt-ftparchive sources . | tee Sources | gzip > Sources.gz
 make_release
 popd
 
